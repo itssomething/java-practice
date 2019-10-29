4fe13156bcb5ad2420485aa32f4cf6952f4e7646
bachpx2 <bachpx@relipasoft.com>
2019-10-29 18:33:26 +0700

    temp2

--- src/javaapplication1/Bai13.java
+++ src/javaapplication1/Bai13.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ */editor
 package javaapplication1;
 
 import java.util.Scanner;
